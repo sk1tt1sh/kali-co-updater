9312fa9035048f2e02bbd375c21fa1d6bd404714
sk1tt1sh <real.sk1tt1sh@gmail.com>
2013-10-18 12:09:48 -0600

    Added more options. not everything works

--- libs/aptgets.py
+++ libs/aptgets.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
+from time import sleep
 from os import system
 
 
 def startApt(aptarg):
 	system('reset')
-	if aptarg == '1':
+	if aptarg == 1:
 		arg = 'update'
 		doApt(arg)
 	elif aptarg == 2:
@@ -18,11 +19,16 @@ def startApt(aptarg):
 	elif aptarg == 4:
 		arg = 'dist-upgrade'
 		doApt(arg)
-	else:
+	elif aptarg == 1234:
+		print "Want to background this? **Note that some processes will be blocked**"
+		print "NOT IMPLEMENTED"
 		return
+	else:
+		return(1)
 
 def doApt(arg):
 	proc = subprocess.Popen(['apt-get %s' % arg, '--force-yes'], stdout=subprocess.PIPE, shell=True)
 	(out, err) = proc.communicate()
 	print out
+	sleep(1)
 	return
